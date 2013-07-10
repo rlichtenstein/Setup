85a3ca89da7cbe5aea31373b276daa0bb39f0709
Rich Lichtenstein <rlichtenstein@gmail.com>
2013-07-10 00:48:56 +0000

    Latest

---
+++ install-node.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y nodejs
+sudo apt-get install -y git-core
+git clone https://github.com/startup-class/setup.git
+./setup/setup.sh
+
+
